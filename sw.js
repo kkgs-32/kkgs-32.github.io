924e1bfb63f9109592ca2d8c468df6406d88dbc5
kkgs-32 <kkgs.pingu@gmail.com>
2025-02-25 21:58:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kkgs-32-v2';
+const CACHE_NAME = 'my-app-cache-v2';
 const BASE_URL = 'https://kkgs-32.github.io';
 const urlsToCache = [
     `${BASE_URL}/`,
@@ -32,11 +32,4 @@ self.addEventListener('activate', (event) => {
         })
     );
     self.clients.claim();
-});
-
-// `CACHE_NAME` を `app.js` に送信
-self.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'GET_CACHE_NAME') {
-        event.ports[0].postMessage(CACHE_NAME);
-    }
 });
